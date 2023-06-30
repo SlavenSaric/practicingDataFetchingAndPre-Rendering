e47b058c65bd9c50920c1da6be084af655242299
SlavenSaric <slavendownload@gmail.com>
2023-06-30 16:32:24 +0200

    first

---
+++ pages/events/[eventId].js
@@ -0,0 +1,56 @@
+import { Fragment } from 'react';
+import { getEventById, getFeaturedEvents } from '../../helpers/api-util';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+import ErrorAlert from '../../components/ui/error-alert';
+import { getAllEvents } from '../../helpers/api-util';
+
+function EventDetailPage(props) {
+  const event = props.event
+
+  if (!event) {
+    return (
+      <div className='center'>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
+}
+
+export async function getStaticProps(context){
+  const eventId = context.params.eventId
+
+  const response = await getEventById(eventId)
+  return {
+    props: {event: response},
+    revalidate: 600
+  }
+}
+
+export async function getStaticPaths(){
+  const events = await getFeaturedEvents()
+
+  const paths = events.map((e) => ({params: {eventId: e.id}}))
+  return {
+    paths: paths,
+    fallback: 'blocking'
+  }
+}
+
+export default EventDetailPage;
